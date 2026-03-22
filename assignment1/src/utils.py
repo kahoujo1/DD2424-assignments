52b4e98304ff858e1a16fa59c59698f798459929
kahoujo1 <kahounjosef3@gmail.com>
2026-03-22 09:30:20 +0100

    implemented dataset loading

---
+++ assignment1/src/utils.py
@@ -0,0 +1,34 @@
+import pickle
+import numpy as np
+
+def load_batch(batchname: str):
+    """
+    Loads data from the CIFAR-10 dataset.
+
+    Args: 
+        batchname (string): the name of the batch to load, e.g. 'data_batch_1'
+
+    Returns:
+        Array of [X, Y, y] where
+        - X contains the flattened image pixel data of shape (32x32x3, N)
+        - Y is the true probability class distribution of shape (K, N) (one hot encoding)'
+        - y is a vector of true labels (N,)
+    """
+    # Load a batch of training data
+    cifar_dir = '../data/cifar-10-batches-py/'
+    with open(cifar_dir + 'data_batch_1', 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    # Extract the image data and cast to float from the dict dictionary
+    X = dict[b'data'].astype(np.float64) / 255.0
+    X = X.transpose()
+    nn = X.shape[1]
+    # Reshape each image from a column vector to a 3d array
+    X_im = X.reshape((32, 32, 3, nn), order='F')
+    X_im = np.transpose(X_im, (1, 0, 2, 3))
+    y = dict[b'labels']
+    y = np.array(y)
+    # Create one hot-encoding of the labels
+    K = 10
+    Y = np.zeros((K, nn))
+    Y[y, np.arange(nn)] = 1
+    return X_im, Y, y
